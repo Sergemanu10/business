8c2b0afca3a835b52f7a81de3d5a11a79088c58e
Serge Manu <firstmanu25@yahoo.fr>
2025-06-04 15:59:17 +0000

    Nouvelles mises à jour

--- backend/views.py
+++ backend/views.py
@@ -1,9 +1,10 @@
+from datetime import datetime
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html', context = {"prenom": "Serge Manu"})
+    return render(request, 'index.html', context = {"date": datetime.today()})
 
 
 def article(request):
--- ebusiness/settings.py
+++ ebusiness/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-FR'
 
 TIME_ZONE = 'UTC'
 
@@ -117,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "ebusiness/static")
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
