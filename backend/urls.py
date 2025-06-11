7fac46c1e7e5acbacfddf40e3b367ef46cb5117f
Serge Manu <firstmanu25@yahoo.fr>
2025-06-11 21:26:03 +0000

    MAJ links

--- backend/urls.py
+++ backend/urls.py
@@ -4,8 +4,8 @@ from backend.views import index, article, categorie, client, vente
 
 urlpatterns = [
     path('', index, name= "index"),
-    path('article/', article, name= "article"),
     path('categorie/', categorie, name= "categorie"),
+    path('article/', article, name= "article"),
     path('client/', client, name= "client"),
     path('vente/', vente, name= "vente"),
 ]
